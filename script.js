a29ecd77c52429e19dd4b4db19150a290b35b526
GTsetsegmaa <gtsetsegm@gmail.com>
2025-11-17 21:15:48 -0800

    Rock Paper Scissors finished

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const CHOICES = ["Rock", "Paper", "Scissors"];
+const RANDOM = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    const i = (Math.random() * 3) | 0;
-    return CHOICES[i];
+    let i = (Math.random() * 3) | 0;
+    return RANDOM[i];
 }
 
-computer = getComputerChoice();
-console.log(computer);
+
+function getHumanChoice() {
+    while (true) {
+        let input = window.prompt("Pick (Rock, Paper, or Scissors:");
+        let choice = input.toLowerCase().trim();
+        return choice;
+    }
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    let score = {player: 0, computer: 0, draws: 0}
+
+    if (humanChoice == computerChoice) {
+        console.log("Tie!");
+        score.draws++;
+    }
+
+    else if (humanChoice === "paper" && computerChoice === "rock" 
+        || humanChoice === "rock" && computerChoice === "scissors" 
+        || humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        score.player++;
+    }
+
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        score.computer++;
+    }
+
+    return score;
+}
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let player = getHumanChoice();
+        let computer = getComputerChoice();
+
+        points = playRound(player, computer);
+        console.log(points);
+    }
+}
+
+playGame();
